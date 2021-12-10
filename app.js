f4512deff109356fce644aa8dd085674b6e2b19a
caroltaus <carolinetaus@hotmail.com>
2021-12-09 21:10:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.send("Hello Toti");
+});
+
+app.listen(PORT, () => {
+    console.log(`App up at port ${PORT}`);
+});
